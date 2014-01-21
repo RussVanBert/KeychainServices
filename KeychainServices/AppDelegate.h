7fed20b8588037bf2f41ac9d6d7f19a7e7b2bf97
RussVanBert <russ_vb@yahoo.com.au>
2014-01-21 23:50:35 +1100

    Add icon images. Minor cleanup.

--- KeychainServices/AppDelegate.h
+++ KeychainServices/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  KeychainServices
-//
-//  Created by Russell Van Bert on 21/01/2014.
-//  Copyright (c) 2014 Russell Van Bert. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
